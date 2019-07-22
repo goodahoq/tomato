350db015bcaddadf19e2470c25c988e5d28939eb
goodahoq <qqused850805@gmail.com>
2019-07-22 09:51:45 +0800

    good

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:goodahoq/tomato.git master:gh-pages
+git push -f git@github.com/goodahoq/tomato.git master:gh-pages
 
 cd -
